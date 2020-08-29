ca3b808a37859e15ffe033152fc94e6fff9bd1f5
Sagar Gawade <56496638+Iamsagar27@users.noreply.github.com>
2020-08-29 15:18:04 +0530

    Add files via upload

---
+++ Assignment_14/Pro1/Main.c
@@ -0,0 +1,46 @@
+/*
+1. Accept number of rows and number of columns from user and display below pattern. 
+
+Input :  iRow = 4  iCol = 3  
+Output :    * * *   
+            * * *   
+            * * *   
+            * * *      
+*/
+
+#include<stdio.h> 
+
+void Pattern(int iRow, int iCol) 
+{
+    int iCnt = 0;
+    int iCnt1 = 0;
+
+    if (iRow < 0 && iCol < 0)
+    {
+        iRow = -iRow;
+        iCol = -iCol;
+    }
+
+    for(iCnt = 1 ; iCnt <= iRow; iCnt++)
+    {
+        printf("\n");
+        for(iCnt1 = 1; iCnt1 <=iCol ; iCnt1++)
+        {
+            printf("*");
+        }
+    }
+
+     
+} 
+
+int main() 
+{  
+    int iValue1 = 0, iValue2 = 0; 
+ 
+    printf("Enter number of rows and columns");  
+    scanf("%d %d",&iValue1, &iValue2); 
+ 
+    Pattern(iValue1, iValue2); 
+ 
+    return 0; 
+} 
